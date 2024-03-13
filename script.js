3386419a6fca535b862fe14923ddfe9ced04431b
RiverCarley <river.carley@ocadu.ca>
2024-03-13 07:14:11 -0400

    working experimant 1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".column").click(function(){
+        console.log("Column clicked"); // Add this line for debugging
         $(this).toggleClass("expanded");
         $(".column").not(this).removeClass("expanded");
     });
