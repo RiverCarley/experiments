d5d9ea640d94dae1d689f1ef9ae7eb40ca426c2b
RiverCarley <river.carley@ocadu.ca>
2024-03-13 07:26:06 -0400

    popup added to experiment 1

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
     $(".column").click(function(){
-        console.log("Column clicked"); // Add this line for debugging
         $(this).toggleClass("expanded");
         $(".column").not(this).removeClass("expanded");
+
+        if ($(this).hasClass("expanded")) {
+            $(this).find(".images img").click(function(){
+                var imageUrl = $(this).attr("src");
+                $("#expanded-image").attr("src", imageUrl);
+                $("#popup").show();
+            });
+        } else {
+            $(".images img").off("click"); // Remove click event handler when column is not expanded
+        }
+    });
+
+    $(".close").click(function(){
+        $("#popup").hide();
     });
 });
